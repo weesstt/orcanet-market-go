84a7c35618c5d3ec9ce734feb093ba3b5908b409
Austin West <austin.w.west@stonybrook.edu>
2024-03-16 15:45:21 -0400

    DHT put value and search

--- market-dht/main.go
+++ market-dht/main.go
@@ -3,41 +3,61 @@
 		https://gist.github.com/upperwal/38cd0c98e4a6b34c061db0ff26def9b9
 		https://ldej.nl/post/building-an-echo-application-with-libp2p/
 		https://github.com/libp2p/go-libp2p/blob/master/examples/chat-with-rendezvous/chat.go
-
+		https://github.com/libp2p/go-libp2p/blob/master/examples/pubsub/basic-chat-with-rendezvous/main.go
 */
-
-///ip4/54.174.250.68/tcp/44981/p2p/QmVicAxtGGNXW4oU3f4686py2sB7csAr6wuUyhR1YXe99a boostrap node
 
 package main
 
 import (
 	"context"
 	"fmt"
-	"bufio"
+	// "bufio"
+	"time"
 	"log"
 	"sync"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/host"
 	// "github.com/libp2p/go-libp2p-net"
 	// "github.com/ipfs/go-datastore"
 	// "github.com/ipfs/go-ipfs-addr"
 	// "github.com/libp2p/go-libp2p-peerstore"
 	// "github.com/ipfs/go-cid"
 	// "github.com/multiformats/go-multihash"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 	"github.com/multiformats/go-multiaddr"
 	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/network"
+	// "github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p-record"
 	"flag"
 )
 
+//Create a record validator to store our own values within our defined protocol
+type OrcaValidator struct {}
+
+//testing, will actually validate later
+func (v OrcaValidator) Validate(key string, value []byte) error{
+	return nil; 
+}
+
+func (v OrcaValidator) Select(key string, value [][]byte) (int, error){
+	return 0, nil; 
+}
+
 func main() {
 	var bootstrapPeer string
-	flag.StringVar(&bootstrapPeer, "b", "", "Specify a bootstrap peer multiaddr")
+	var searchKey string
+	var putKey string
+	var putValue string 
+	var isClient bool
+	flag.StringVar(&bootstrapPeer, "bootstrap", "", "Specify a bootstrap peer multiaddr")
+	flag.StringVar(&searchKey, "searchKey", "", "Search for a key repeatedly until found")
+	flag.StringVar(&putKey, "putKey", "", "Put a value into the DHT with this key, must specify value too")
+	flag.StringVar(&putValue, "putValue", "", "Put a value into the DHT, must specify key too")
+	flag.BoolVar(&isClient, "clientMode", false, "Specify client or server, true for client")
 	flag.Parse()
-	if(bootstrapPeer == ""){
-		panic("A bootstrap peer must be provided!");
-	}
 
 	ctx := context.Background()
 	
@@ -48,7 +68,7 @@ func main() {
 	}
 
 	//Construct multiaddr from string and create host to listen on it
-	sourceMultiAddr, _ := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/0")
+	sourceMultiAddr, _ := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/44981")
 	opts := []libp2p.Option{
 		libp2p.ListenAddrStrings(sourceMultiAddr.String()),
 		libp2p.Identity(privKey), //derive id from private key
@@ -57,23 +77,31 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	host.SetStreamHandler("orcanet/1.0.0", handleStream)
+
 	log.Printf("Host ID: %s", host.ID())
 	log.Printf("Connect to me on:")
 	for _, addr := range host.Addrs() {
-		log.Printf("  %s/p2p/%s", addr, host.ID())
+		log.Printf("%s/p2p/%s", addr, host.ID())
 	}
 
 	//An array if we want to expand to a more stable peer list instead of providing in args
 	bootstrapPeers := []string {
 		bootstrapPeer,
-	}
+	} 
 
 	// Start a DHT, for use in peer discovery. We can't just make a new DHT
 	// client because we want each peer to maintain its own local copy of the
 	// DHT, so that the bootstrapping node of the DHT can go down without
 	// inhibiting future peer discovery.
-	kDHT, err := dht.New(ctx, host)
+	var validator record.Validator = OrcaValidator{}
+	var options []dht.Option
+	if(isClient){ //if no bootstrap peer, go into server mode
+		options = append(options, dht.Mode(dht.ModeClient))
+	}else{
+		options = append(options, dht.Mode(dht.ModeServer))
+	}
+	options = append(options, dht.ProtocolPrefix("orcanet/market"), dht.Validator(validator));
+	kDHT, err := dht.New(ctx, host, options...)
 	if(err != nil){
 		panic(err);
 	}
@@ -89,6 +117,7 @@ func main() {
 	// other nodes in the network.
 	var wg sync.WaitGroup
 	for _, peerAddrString := range bootstrapPeers {
+		if(peerAddrString == ""){continue;}
 		peerAddr, err := multiaddr.NewMultiaddr(peerAddrString)
 		if(err != nil){
 			panic(err)
@@ -105,31 +134,64 @@ func main() {
 		}()
 	}
 	wg.Wait()
+	
+	go discoverPeers(ctx, host, kDHT, "orcanet/market");
+	time.Sleep(5 * time.Second)
+
+	if(putKey != ""){
+		for {
+			err = kDHT.PutValue(ctx, "orcanet/market/" + putKey, []byte(putValue))
+			if(err != nil){
+				fmt.Println("Error: ", err)
+				time.Sleep(5 * time.Second)
+				continue;
+			}
+			fmt.Println("Put key: ", putKey + " Value: " + putValue)
+			break;
+		}
+	}
+	
+	if(searchKey != ""){
+		for {
+			valueStream, err := kDHT.SearchValue(ctx, "orcanet/market/" + searchKey)
+			fmt.Println("Searching for " + searchKey)
+			if(err != nil){
+				fmt.Println("Error: ", err)
+				time.Sleep(5 * time.Second)
+				continue;
+			}
+			time.Sleep(5 * time.Second)
+			for byteArray := range valueStream {
+				fmt.Println(string(byteArray));
+			}
+		}
+	}
 
 	select {}
 }
 
-func handleStream(stream network.Stream) {
-	log.Println("Got a new stream!")
+func discoverPeers(ctx context.Context, h host.Host, kDHT *dht.IpfsDHT, advertise string) {
+	routingDiscovery := drouting.NewRoutingDiscovery(kDHT)
+	dutil.Advertise(ctx, routingDiscovery, advertise)
 
-	// Create a buffer stream for non-blocking read and write.
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-
-	go readData(rw)
-
-	// 'stream' will stay open until you close it (or the other side closes it).
-}
-
-func readData(rw *bufio.ReadWriter) {
+	// Look for others who have announced and attempt to connect to them
 	for {
-		str, _ := rw.ReadString('\n')
-		if str == "" {
-			return
+		fmt.Println("Searching for peers...")
+		peerChan, err := routingDiscovery.FindPeers(ctx, advertise)
+		if err != nil {
+			panic(err)
 		}
-		if str != "\n" {
-			// Green console colour: 	\x1b[32m
-			// Reset console colour: 	\x1b[0m
-			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		for peer := range peerChan {
+			if peer.ID == h.ID() {
+				continue // No self connection
+			}
+			err := h.Connect(ctx, peer)
+			if err != nil {
+				fmt.Printf("Failed connecting to %s, error: %s\n", peer.ID, err)
+			} else {
+				fmt.Println("Connected to:", peer.ID)
+			}
 		}
+		time.Sleep(time.Second * 10)
 	}
 }
